9340d0794dc6ce60753c4f6afa88164ebeff1b71
Naftali Beder <naftali@naftalibeder.com>
2021-03-24 12:37:33 -0500

    Finish basic template setup

--- MapView.js
+++ MapView.js
@@ -1,52 +1,40 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 
-const latitude = 40.723279;
-const longitude = -73.970895;
-
 const MapView = () => {
-  const point = {
+  const latitude = 40.723279;
+  const longitude = -73.970895;
+
+  const centerPoint = {
     "type": "Point",
     "coordinates": [longitude, latitude],
   };
 
   return (
     <MapboxGL.MapView
-      ref={(c) => (this._map = c)}
       onLayout={(e) => {}}
       onPress={this.onPress}
       style={{ flex: 1 }}
     >
       <MapboxGL.Camera
+        centerCoordinate={centerPoint.coordinates}
         zoomLevel={9}
       />
 
       <MapboxGL.ShapeSource
-        id={'source'}
-        shape={point}
+        id={'circleSource'}
+        shape={centerPoint}
       >
         <MapboxGL.CircleLayer
-          id={'circle'}
+          id={'circleLayer'}
           style={{
-            circleRadius: 10,
-            circleColor: 'green',
-            circleStrokeColor: 'black',
-            circleStrokeWidth: 1,
+            circleRadius: 6,
+            circleColor: '#f74d91',
+            circleStrokeColor: 'white',
+            circleStrokeWidth: 2,
           }}
         />
       </MapboxGL.ShapeSource>
-
-      <View style={{
-        flex: 0,
-        flexDirection: 'row',
-        position: 'absolute',
-        bottom: 0,
-        padding: 30,
-        width: '100%',
-      }}>
-        TODO
-      </View>
     </MapboxGL.MapView>
   );
 }
